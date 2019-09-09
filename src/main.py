5bbd7ee2a3b1f3069f1e6e8092da570558efbaeb
Thales Mateus Rodrigues Oliveira <ra148051@students.ic.unicamp.br>
2019-09-09 13:05:10 -0300

    test image read/write

--- src/main.py
+++ src/main.py
@@ -1,6 +1,24 @@
+import cv2
 from halftoning import halftoning
 
 def main():
     """
     Entrypoint for the code of project 01 MO443/2s2019
     """
+    print("Hey!")
+    images = [
+        'input/baboon_colored.png',
+        'input/monalisa_colored.png',
+        'input/peppers_colored.png',
+        'input/watch_colored.png'
+    ]
+
+    for image_name in images:
+        image = cv2.imread(image_name)
+        #print(image)
+        image_grayscale = cv2.imread(image_name, cv2.IMREAD_GRAYSCALE)
+        print(image_grayscale)
+        #save grayscale image
+        cv2.imwrite('output/test.png', image_grayscale)
+
+main()

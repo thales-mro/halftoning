379a28a25ae7dfac0cf204422e64b2bc43903e3d
Thales Mateus Rodrigues Oliveira <ra148051@students.ic.unicamp.br>
2019-09-09 13:19:45 -0300

    built basic main function to deal with input/output

--- src/main.py
+++ src/main.py
@@ -1,24 +1,49 @@
 import cv2
 from halftoning import halftoning
 
+def open_image(name, grayscale=0):
+    """
+    it makes calls for openCV functions for reading an image based on a name
+
+    Keyword arguments:
+    name -- the name of the image to be opened
+    grayscale -- whether image is opened in grayscale or not
+        0 (default): image opened normally (with all 3 color channels)
+        1: image opened in grayscale form
+    """
+    img_name = 'input/' + name + '_colored' + '.png'
+    if grayscale:
+        return cv2.imread(img_name, cv2.IMREAD_GRAYSCALE)
+    return cv2.imread(img_name)
+
+def save_image(name, image):
+    """
+    it makes calls for openCV function for saving an image based on a name (path)
+    and the image itself
+
+    Keyword arguments:
+    name -- the name (path) of the image to be saved
+    image -- the image itself (numpy array)
+    """
+    image_name = 'output/' + name + '.png'
+    cv2.imwrite(image_name, image)
+
 def main():
     """
     Entrypoint for the code of project 01 MO443/2s2019
     """
     print("Hey!")
     images = [
-        'input/baboon_colored.png',
-        'input/monalisa_colored.png',
-        'input/peppers_colored.png',
-        'input/watch_colored.png'
+        'baboon',
+        'monalisa',
+        'peppers',
+        'watch'
     ]
 
     for image_name in images:
-        image = cv2.imread(image_name)
-        #print(image)
-        image_grayscale = cv2.imread(image_name, cv2.IMREAD_GRAYSCALE)
-        print(image_grayscale)
+        image = open_image(image_name)
+        image_grayscale = open_image(image_name, 1)
         #save grayscale image
-        cv2.imwrite('output/test.png', image_grayscale)
+        save_image('test', image_grayscale)
 
 main()
